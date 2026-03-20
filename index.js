bc4f84d38315d9e3dca8f6773b4c5e0a22bb3d75
Jose Luis Astorga Morillo <joseluastorga97@gmail.com>
2026-03-20 20:49:07 +0100

    Cambios para publicar el proyecto en vercel

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { startServer } = require('@evershop/evershop');
-const path = require('path');
 
-// Esto arranca el motor de Evershop en el entorno de Vercel
-startServer(path.resolve(__dirname));
+// Usamos process.cwd() para asegurar que encuentre la carpeta .evershop y las extensiones
+startServer(process.cwd());
