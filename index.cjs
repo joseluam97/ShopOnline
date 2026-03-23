8f6a517413e3610ae1c8b152ebc74e167c7ecbbb
Jose Luis Astorga <astorgam.joseluis@gmail.com>
2026-03-23 19:42:33 +0100

    Cambios para publicar el proyecto en vercel

--- index.cjs
+++
@@ -1,5 +0,0 @@
-const { startServer } = require('@evershop/evershop');
-
-// En CommonJS (.cjs), no necesitamos 'import.meta' ni 'fileURLToPath'. 
-// Simplemente usamos el comando de arranque estándar de Evershop.
-startServer(process.cwd());
